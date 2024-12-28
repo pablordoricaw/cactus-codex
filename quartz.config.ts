d789d71574743dc9fef7397b60c567bf2a3b6729
Pablo Ordorica Wiener <pablo_ordoricaw@hotmail.com>
2024-12-28 15:12:33 -0500

    feat: set pageTitle and baseUrl to GitHub pages link

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "pablordoricaw.github.io/cactus-codex/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
